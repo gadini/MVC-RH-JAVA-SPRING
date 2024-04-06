2bb59e3be19c6edfc4909f527bb322653d1533da
Logon PF Fiap <logonpflocal@fiap.com.br>
2024-04-05 21:01:39 -0300

    Add entities

---
+++ src/main/java/com/github/acnaweb/mvc_rh/model/Cargo.java
@@ -0,0 +1,45 @@
+package com.github.acnaweb.mvc_rh.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, length = 50, unique = true)
+	private String nome;
+
+	@ManyToOne
+	@JoinColumn(name = "id_departamento_fk")
+	private Departamento departamento;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
---
+++ src/main/java/com/github/acnaweb/mvc_rh/model/Departamento.java
@@ -0,0 +1,39 @@
+package com.github.acnaweb.mvc_rh.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "departamentos")
+public class Departamento implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, length = 60, unique = true)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
