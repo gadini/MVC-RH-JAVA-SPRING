d4aa0c099f4fb505f34b19d5e2a50b4fe7c4d24b
Logon PF Fiap <logonpflocal@fiap.com.br>
2024-04-19 22:43:41 -0300

    Add pages

---
+++ src/main/java/com/github/acnaweb/mvc_rh/controller/DepartamentoController.java
@@ -0,0 +1,20 @@
+package com.github.acnaweb.mvc_rh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("departamentos")
+public class DepartamentoController {
+
+	@GetMapping("list")
+	public String list() {
+		return "departamento/list";
+	}
+	
+	@GetMapping("form")
+	public String form() {
+		return "departamento/form";
+	}
+}
